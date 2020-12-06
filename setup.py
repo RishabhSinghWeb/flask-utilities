2efb03f35ce1f43c99973f449f5dfb6fe88d5786
Rishabh Singh <dailylifesking@gmail.com>
2020-12-07 00:54:05 +0530

    a

--- setup.py
+++ setup.py
@@ -9,47 +9,52 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100))
-    complete = db.Column(db.Boolean)
+	id = db.Column(db.Integer, primary_key=True)
+	title = db.Column(db.String(100))
+	complete = db.Column(db.Boolean)
 
 
 @app.route("/")
 def home():
-    return render_template("base.html", todo_list=Todo.query.all())
+	return render_template("base.html", todo_list=Todo.query.all())
 
 
 @app.route("/add", methods=["POST"])
 def add():
 	passw=request.form.get("pass")
 	if passw:
-	    db.session.add(Todo(title=passw, complete=False))
-	    db.session.commit()
+		db.session.add(Todo(title=passw, complete=False))
+		db.session.commit()
 	return redirect(url_for("home"))
 
 @app.route("/<int:todo_id>")
 def update(todo_id):
-    todo = Todo.query.filter_by(id=todo_id).first()
-    todo.complete = not todo.complete
-    db.session.commit()
-    return redirect(url_for("home"))
+	todo = Todo.query.filter_by(id=todo_id).first()
+	todo.complete = not todo.complete
+	db.session.commit()
+	return redirect(url_for("home"))
 
 
 @app.route("/del/<int:todo_id>")
 def delete(todo_id):
-    db.session.delete(Todo.query.filter_by(id=todo_id).first())
-    db.session.commit()
-    return redirect(url_for("home"))
+	db.session.delete(Todo.query.filter_by(id=todo_id).first())
+	db.session.commit()
+	return redirect(url_for("home"))
 
 
 @app.route("/a")
 def a():
-    return render_template("a.html")
+	return render_template("a.html")
 
 @app.route("/aa", methods=["POST"])
 def aa():
-    return redirect("/")
+	return redirect("/")
 
 if __name__ == "__main__":
-    db.create_all()
-    app.run(host="127.0.0.1", port=80, threaded=True,debug=False)
+	with open('./log', 'a+') as log:
+		try:
+			db.create_all()
+			app.run(host="127.0.0.1", port=80, threaded=True, debug=False)
+			log.write("done adding wsgi app\n")
+		except Exception:
+			log.write(repr(e))
